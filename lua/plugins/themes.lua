9df85eb85477639a38c8b9bd465bdd07152b5222
Kevin S Kirkup <kevin.kirkup@gmail.com>
2024-04-17 15:34:56 -0400

    Updates to astronvim and bug fixex

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,9 +4,9 @@ return {
     -- lazy = false,
     priority = 1000,
     opts = {
-      style = "storm",                 -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+      style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
       transparent = vim.g.neovide == nil, -- Enable this to disable setting the background color
-      terminal_colors = true,          -- Configure the colors used when opening a `:terminal` in Neovim                                                                                                  |
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim                                                                                                  |
       styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value `:help attr-list`
@@ -24,11 +24,11 @@ return {
         "vista_kind",
         "terminal",
         "neo-tree",
-      },                             -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`                                                      |
-      day_brightness = 0.3,          -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors                                                  |
+      }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`                                                      |
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors                                                  |
       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**. |
-      dim_inactive = false,          -- dims inactive windows
-      lualine_bold = false,          -- When `true`, section headers in the lualine theme will be bold|
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold|
       on_highlights = function(hl, _)
         hl.TSRainbowRed = { link = "rainbowcol1" }
         hl.TSRainbowOrange = { link = "rainbowcol2" }
@@ -51,10 +51,9 @@ return {
   -- { "jarodl/cobalt.vim", opt = true },
   -- { "travisjeffery/vim-colors", opt = true },
   -- { "arcticicestudio/nord-vim", opt = true },
-  { "sainnhe/sonokai", opts = true },
+  -- { "sainnhe/sonokai", opts = true },
   {
     "mhartington/oceanic-next",
     priority = 1000,
-    opts = true,
   },
 }
